fee58a7fc7f36d0c9795c94b7c78323862326b5e
FaustZP <gorets92@gmail.com>
2022-06-03 17:32:06 +0300

    Add files via upload

---
+++ ex-2_for/index.js
@@ -0,0 +1,8 @@
+for (let index = 0; index <= 100; index++) {
+  if (index % 2 == 0) {
+    console.log(`Парне число: ${index}`);
+  }
+  if (index % 3 == 0) {
+    console.log(`Ділиться на 3: ${index}`);
+  }
+}
